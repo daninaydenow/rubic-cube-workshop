cc64c29bdcf2f4c461f7a945184b57f883ba82f8
Daniel Naydenov <daninaydenow@gmail.com>
2021-10-01 16:42:34 +0300

    Render home page with actual info

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -1,9 +1,26 @@
+const uniqid = require('uniqid');
 class Cube {
+    static #cubes = [
+        {
+            id: 'asdasdasdasdakljvlajs;da',
+            name: 'Mirror Cube',
+            description: 'Strange cube',
+            imageUrl: 'https://m.media-amazon.com/images/I/41KNQRXAYvL._AC_SY580_.jpg',
+            difficulty: '4'
+          }
+    ];
     constructor(name, description, imageUrl, difficulty) {
+        this.id = uniqid();
         this.name = name;
         this.description = description;
         this.imageUrl = imageUrl;
         this.difficulty = difficulty;
+    }
+    static getAll() {
+         return Cube.#cubes.slice();
+    }
+    static add(cube) {
+        Cube.#cubes.push(cube);
     }
 }
 
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,18 +1,10 @@
 const Cube = require('../models/Cube');
-const cubeDb = [
-      {
-        name: 'Mirror Cube',
-        description: 'Strange cube',
-        imageUrl: 'https://m.media-amazon.com/images/I/41KNQRXAYvL._AC_SY580_.jpg',
-        difficulty: '4'
-      }
-];
 
-const getAll = () => cubeDb.slice();
+const getAll = () => Cube.getAll();
 
 const create = (name, description, imageUrl, difficulty) => {
       const cube = new Cube(name, description, imageUrl, difficulty);
-      cubeDb.push(cube);
+      Cube.add(cube);
 };
 
 const cubeService = {
