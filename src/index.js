b1c6099defd14ed0d36c98cf5f9463c3e80fd6b0
Daniel Naydenov <daninaydenow@gmail.com>
2021-10-01 13:02:34 +0300

    resolved an issue with displaying css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
 const initHandlebars = require('./config/handlebars');
 
 const app = express();
 initHandlebars(app);
 
+// require('./config/handlebars')(app);
+app.use(express.static(path.resolve(__dirname, './public')));
+
 app.all('/', (req, res) => {
-    res.render('index', {layout: false});
+    res.render('index');
 });
 
 app.listen(5000, () => console.log(`Application is running on http://localhost:5000`));
