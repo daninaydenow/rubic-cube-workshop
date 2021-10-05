a669e68fe0e36535eb3d238b998cde4f4e9198e3
Daniel Naydenov <daninaydenow@gmail.com>
2021-10-05 10:04:34 +0300

    created a Cube mongoose model

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const path = require('path');
-const config = require('./config/config.json');
-const initHandlebars = require('./config/handlebars');
+const config = require('./config/config.json')[process.env.NODE_ENV];
 const routes = require('./routes');
+const initDataBase = require('./config/database');
 
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
-initHandlebars(app);
+require('./config/handlebars')(app);
 
 // require('./config/handlebars')(app);
 app.use(express.static(path.resolve(__dirname, './public')));
 app.use(routes);
 
+initDataBase(config.DB_CONNECTION_STRING)
+.then(() => {
+    app.listen(config.PORT, () => console.log(`Application is running on http://localhost:${config.PORT}`));
+})
+.catch(err => {
+    console.log('Application failed: ', err);
+})
 
 
-app.listen(5000, () => console.log(`Application is running on http://localhost:5000`));
--- src/models/Cube.js
+++ src/models/Cube.js
@@ -1,41 +1,36 @@
-const uniqid = require('uniqid');
-class Cube {
-    static #cubes = [
-        {
-            id: 'asdasdasdasdakljvlajs;da',
-            name: 'Mirror Cube',
-            description: 'Strange cube',
-            imageUrl: 'https://m.media-amazon.com/images/I/41KNQRXAYvL._AC_SY580_.jpg',
-            difficulty: '4'
-          },
-          {
-            id: 'as9d87a8787wye81o123',
-            name: 'Ice Cube',
-            description: 'Best rapper ITW',
-            imageUrl: 'https://api.time.com/wp-content/uploads/2015/08/ice-cube-straight-outta-compton1.jpg?quality=85&w=1024&h=512&crop=1',
-            difficulty: '6'
-          },
-          {
-            id: 'axgku9uv5vb',
-            name: 'Cute Cube',
-            description: 'mega cute cube',
-            imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSl4ka3MG1b1bUNrSQ0imTdlzTHhQXVEQ9Iuw&usqp=CAU',
-            difficulty: '3'
-          }
-    ];
-    constructor(name, description, imageUrl, difficulty) {
-        this.id = uniqid();
-        this.name = name;
-        this.description = description;
-        this.imageUrl = imageUrl;
-        this.difficulty = difficulty;
+const mongoose = require('mongoose');
+const cubeSchema = new mongoose.Schema({
+    name: {
+       type: String,
+       required: true
+    },
+    description: {
+       type: String,
+       required: true,
+       maxlength: 100
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+      // validate: /^https?:\/\//i
+      validate: {
+        validator: function(v) {
+          return /^https?:\/\//i.test(v);
+        },
+        message: 'Image URL is invalid'
+      }
+    },
+    difficultyLevel: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 6,
     }
-    static getAll() {
-         return Cube.#cubes.slice();
-    }
-    static add(cube) {
-        Cube.#cubes.push(cube);
-    }
-}
 
+});
+
+// cubeSchema.path('imageUrl').validate = function(value) {
+//   return /^https?:\/\//i.test(v);
+// }
+const Cube = mongoose.model('Cube', cubeSchema);
 module.exports = Cube;
