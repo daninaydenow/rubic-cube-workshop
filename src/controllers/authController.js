cc1b99b534d19066eb9a79ea431a64ffce903cc1
Daniel Naydenov <daninaydenow@gmail.com>
2021-10-08 18:59:54 +0300

    Add register Page

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,4 +4,12 @@ router.get('/login', (req, res) => {
      res.render('auth/login');
 })
 
+router.post('/login', (req, res) => {
+    console.log(req.body);
+    res.redirect('/login');
+})
+
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+})
 module.exports = router;
