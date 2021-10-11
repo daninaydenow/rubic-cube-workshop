39e667443d096bc43407ace365a389cae56b5060
Daniel Naydenov <daninaydenow@gmail.com>
2021-10-11 11:30:19 +0300

    Attach accessory to cube

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,4 +1,5 @@
 const Cube = require('../models/Cube');
+const Accessory = require('../models/Accessory');
 
 const getAll = () => Cube.find({}).lean();
 
@@ -28,12 +29,20 @@ const search = (text, from, to) => {
     return result;
 }
 
+const attachAccessory = async (cubeId, accessoryId) => {
+    let cube = await Cube.findById(cubeId);
+    let accessory = await Accessory.findById(accessoryId);
+    cube.accessories.push(accessory);
+    return cube.save();
+}
+
 
 const cubeService = {
     create,
     getAll,
     getOne,
-    search
+    search,
+    attachAccessory
 }
 
 module.exports = cubeService;
